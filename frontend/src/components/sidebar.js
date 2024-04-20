dbdf9393772579578e243d13eff6b7880ed3ce83
Sarah Singh <sarah.h.singh@gmail.com>
2024-04-20 05:22:46 -0700

    Add right-aligned sidebar

---
+++ frontend/src/components/sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './sidebar.css';
+
+const sidebar = () => {
+    return (
+        <div className = "sidebar">
+            <div className = "sidebar-content">
+                <button>Button 1</button>
+                <button>Button 2</button>
+                <button>Button 3</button>
+            </div>
+        </div>
+    );
+};
+
+export default sidebar;

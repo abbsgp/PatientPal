6b7c13faac87b67751c8373b14384c067453e31b
AndrewLao <43099770+AndrewLao@users.noreply.github.com>
2024-04-20 15:50:33 -0700

    routing

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom"
 import './sidebar.css';
 
-const sidebar = () => {
+const Sidebar = () => {
+    const navigate = useNavigate();
+
+    const homePage = () => {
+        navigate("/")
+    }
+    const labsPage = () => {
+        navigate("/labs")
+    }
+    const medsPage = () => {
+        navigate("/meds")
+    }
+    const msgsPage = () => {
+        navigate("/msgs")
+    }
     return (
         <div className = "sidebar">
             <div className = "sidebar-content">
-                <button>Home</button>
-                <button>Labs</button>
-                <button>Medication</button>
-                <button>Messages</button>
+                <button onClick={homePage}>Home</button>
+                <button onClick={labsPage}>Labs</button>
+                <button onClick={medsPage}>Medication</button>
+                <button onClick={msgsPage}>Messages</button>
             </div>
         </div>
     );
 };
 
-export default sidebar;
+export default Sidebar;

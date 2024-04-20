876032d3c8dac06d098bdc050ad312ffc8db2735
abbsgp <abegailpalad7812@gmail.com>
2024-04-20 02:16:09 -0700

    added components folder in src, created a mockup of a home page, initial start up at home page implemented, and changed the tab name from React App to PatientPal

---
+++ frontend/src/components/home.js
@@ -0,0 +1,13 @@
+// components/home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is the home page content.</p>
+    </div>
+  );
+}
+
+export default Home;

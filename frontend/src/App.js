725b16cb2074ce5c764f740d8d2889d36683f1d4
Sarah Singh <sarah.h.singh@gmail.com>
2024-04-20 05:20:44 -0700

    Add right-aligned sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 // App.js
 import React from 'react';
 import Home from './pages/home';
+import Sidebar from './components/sidebar'
 
 
 function App() {
   return (
     <div>
       <Home />
+      <Sidebar />
     </div>
   );
 }

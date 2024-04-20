e195362e0669e43c696e0d04c83ea2276ebf8af6
Sarah Singh <sarah.h.singh@gmail.com>
2024-04-20 03:07:23 -0700

    add header and page links

---
+++ frontend/src/pages/labs.js
@@ -0,0 +1,11 @@
+import React  from "react";
+
+function Labs() {
+    return (
+        <div>
+            <h1>This is lab page</h1>
+        </div>
+    );
+};
+
+export default Labs;
---
+++ frontend/src/pages/meds.js
@@ -0,0 +1,11 @@
+import React  from "react";
+
+function Meds() {
+    return (
+        <div>
+            <h1>This is med page</h1>
+        </div>
+    );
+};
+
+export default Meds;

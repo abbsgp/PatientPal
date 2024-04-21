39d28ee1dadb8560e90b258fa37264f41a35f76c
Tanvi <54735473+sgtanvi@users.noreply.github.com>
2024-04-20 17:20:12 -0700

    bare minium working chat function

--- backend/gemini.py
+++ backend/gemini.py
@@ -2,7 +2,24 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import os 
 
+# Load API key from .env file
 load_dotenv()
+api_key = os.getenv("GEMINI_APIKEY")
+# print("API Key:", api_key)
+genai.configure(api_key=api_key)
 
-genai.configure(api_key=os.environ["GEMINI_APIKEY"])
-model = genai.GenerativeModel('gemini')
+userInput = "say hi to me"
+
+def say_hi():
+  """Uses Gemini API to generate a simple greeting."""
+  model = genai.GenerativeModel('gemini-pro')
+  chat = model.start_chat(history=[])
+
+  # Simple prompt 
+  response = chat.send_message(userInput)
+
+  print(response.text)
+
+
+if __name__ == "__main__":
+    say_hi()

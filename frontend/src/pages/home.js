089fec0cd21bd9ea2b7fc1132b416df8f2f31558
abbsgp <abegailpalad7812@gmail.com>
2024-04-20 17:03:07 -0700

    moved css styles from header to home css file, created containers to be present in labs.js, and redesigned UI spacing

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,23 +1,31 @@
 import React  from "react";
-import Header from '../components/header';
 import "./home.css"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Hello, Asta!</h1>
+        </header>
+    );
+};
 
 function Home() {
     
     return (
 
+        <><div className="page_head"></div>
         <><Header /><div className="chat-container">
-              <div className="chat-header">
-                  <h2>Chat AI</h2>
-              </div>
-              <div className="chat-mes sages">
-                  {/* Display chat messages */}
-              </div>
-              <div className="chat-input">
-                  <input type="text" placeholder="Type your message..." />
-                  <button>Send</button>
-              </div>
-          </div></>
+            <div className="chat-header">
+                <h2>Chat AI</h2>
+            </div>
+            <div className="chat-mes sages">
+                {/* Display chat messages */}
+            </div>
+            <div className="chat-input">
+                <input type="text" placeholder="Type your message..." />
+                <button>Send</button>
+            </div>
+        </div></></>
       );
 
 };
